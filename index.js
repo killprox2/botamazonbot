4100011496faa37bcfc94adeaa26cb9a75892f6f
killprox2 <43413557+killprox2@users.noreply.github.com>
2024-09-21 16:46:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function saveCache() {
 // Démarrer la surveillance
 async function startMonitoring() {
   logMessage('Démarrage de la surveillance des produits Amazon...');
-  const generalSearchUrl = 'https://www.amazon.fr/s?k=all';
+  const generalSearchUrl = 'https://www.amazon.fr/deals';
 
   productCache.set(generalSearchUrl, 'EDP');
   productCache.set(generalSearchUrl, 'Autre_vendeur');
